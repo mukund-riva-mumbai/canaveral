51c151b6287c8505245fdae4bf6741f2fd1f90db
mukund-riva-mumbai <mukund.dh@rivadigital.com>
2016-02-15 11:16:58 +0530

    Adding creation of .job files to the launcher

--- source/canaveral/maya_render_launcher.py
+++ source/canaveral/maya_render_launcher.py
@@ -65,78 +65,18 @@ class MayaRenderLauncher(maya_deadline.Ui_deadline_launch_win, QtGui.QMainWindow
         self.file_path_box.setText(fname.replace("/", "\\"))
 
     def build_deadline_cmd(self):
-        deadline_install_dir = "C:\\Program Files\\Thinkbox\\Deadline\\bin\\deadlinecommand.exe -SubmitCommandLineJob"
-        launch_exec = " -executable "
-
-        if self.file_path_box.text() == "":
-            print "Please pass a proper file path in the file path box."
-            return
-
-        launch_bat_file = os.path.dirname(self.file_path_box.text()) + "\\render_bats\\" + os.path.basename(self.file_path_box.text())[:-3] + ".bat"
-        launch_args = " -arguments "
-
-        ren_name, ren_ver = str(self.renderer_selector_combo.currentText()).split("_")
-        env_str = launch_app.set_env_vars(str(self.project_selector_combo.currentText()), self.master_app_dict, "Maya", "2014", ren_name, ren_ver, False)
-        env_str_list = []
-        ren_lyr_list = []
-
-        env_str += "\"C:\\Program Files\\Autodesk\\Maya2014\\bin\\render.exe\" -s %3 -e %4 -rd %2"
-
-        launch_args += "\""
-
-        file_to_open = str(self.file_path_box.text())
-        launch_args += "<QUOTE>%s<QUOTE>" % file_to_open
-
-        output_dir = str(self.output_path_box.text())
-        launch_args += " <QUOTE>%s<QUOTE>" % output_dir
-
-        start_frame = str(self.start_frame_spnr.value())
-        end_frame = str(self.end_frame_spnr.value())
-        launch_args += " %s %s" % (start_frame, end_frame)
-
-        flag_count = 4
-        if str(self.render_cam_box.text()):
-            flag_count += 1
-            launch_args += " %s" % str(self.render_cam_box.text())
-            env_str += " -cam %5"
-
-        if str(self.render_layer_box.text()):
-            for each in str(self.render_layer_box.text()).split(";"):
-                env_str_list.append(env_str + " -rl %" + str(flag_count+1))
-                ren_lyr_list.append(each)
-
-        if len(ren_lyr_list):
-            for lyr_id in xrange(0, len(ren_lyr_list)):
-                bat_file_name = os.path.basename(file_to_open)[:-3]
-                bat_file_name += "." + ren_lyr_list[lyr_id] + ".bat"
-                launch_file = os.path.dirname(launch_bat_file) + "\\" + bat_file_name
-                if not os.path.exists(os.path.dirname(launch_bat_file)):
-                    os.makedirs(os.path.dirname(launch_bat_file))
-                ren_bat_f = open(launch_file, "w")
-                ren_bat_f.write(env_str_list[lyr_id] + " %1")
-                ren_bat_f.close()
-
-                deadline_cmd = deadline_install_dir + launch_exec + launch_file + launch_args + " " + ren_lyr_list[lyr_id] + "\""
-                #deadline_cmd += " -frames %s-%s" % (start_frame, end_frame)
-                deadline_cmd += " -name %s" % bat_file_name[:-4]
-                if str(self.render_pool_box.text()):
-                    deadline_cmd += " -pool %s" % str(self.render_pool_box.text())
-                deadline_cmd += " -prop OutputDirectory0=\"%s\"" % output_dir
-                subprocess.Popen(deadline_cmd)
-        else:
-            if not os.path.exists(os.path.dirname(launch_bat_file)):
-                os.makedirs(os.path.dirname(launch_bat_file))
-            ren_bat_f = open(launch_bat_file, "w")
-            ren_bat_f.write(env_str + " %1")
-            ren_bat_f.close()
-
-            deadline_cmd = deadline_install_dir + launch_exec + launch_bat_file + launch_args + "\""
-            deadline_cmd += " -frames %s-%s" % (start_frame, end_frame)
-            deadline_cmd += " -name %s" % os.path.basename(launch_bat_file)[:-4]
-            if str(self.render_pool_box.text()):
-                deadline_cmd += " -pool %s" % str(self.render_pool_box.text())
-            deadline_cmd += " -prop OutputDirectory0=\"%s\"" % output_dir
-            subprocess.Popen(deadline_cmd)
+        # Build two deadline ".job" files; one for the basic params
+        # and the other for the plugin params.
+        # Basic params will have:
+        # Plugin=RivaBatch
+        # ForceReloadPlugin=false
+        # Frames=
+        # Priority=
+        # Pool=
+        # Name=
+        # Comment=
+        # OutputDirectory0=
+        deadline_install_dir = "C:\\Program Files\\Thinkbox\\Deadline\\bin\\deadlinecommand.exe"
 
 
 if __name__ == "__main__":
